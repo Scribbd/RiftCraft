34f64c1d8fa2c0f455dd124d350bc8a9638062c1
Scriblon <coenmeulenkamp@gmail.com>
2014-07-31 14:07:42 +0200

    Squashed commit of the following:
    commit 8ea56410ae042f969f67d7a21d1b7b135ac241a1
    Author: Scriblon <coenmeulenkamp@gmail.com>
    Date:   Thu Jul 31 13:50:15 2014 +0200
    
        ToBeMerged

---
+++ src/main/java/nl/scribblon/riftcraft/event/EntityItemDeathEvent.java
@@ -0,0 +1,22 @@
+package nl.scribblon.riftcraft.event;
+
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.item.ItemEvent;
+
+/**
+ * Created by Scribblon for RiftCraft.
+ * Date Creation: 31-7-2014
+ *
+ * Event launched whenever an ItemEntity dies
+ */
+public class EntityItemDeathEvent extends ItemEvent{
+
+    public final DamageSource damageSource;
+
+    public EntityItemDeathEvent(EntityItem itemEntity, DamageSource damageSource) {
+        super(itemEntity);
+        this.damageSource = damageSource;
+    }
+}
